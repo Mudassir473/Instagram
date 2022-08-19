3467ca6ad89bde09c072602c3c7d7193a3f6f12b
Mudassir473 <105499767+Mudassir473@users.noreply.github.com>
2022-08-19 13:41:34 +0530

    Delete script/storage directory

--- script/storage/framework/views/9a13af579babb2c2e7264ddab724ef109ff37420.php
+++
@@ -1,8 +0,0 @@
-<form id="report_form" action="<?php echo e(route('report')); ?>" method="POST">
-	<?php echo csrf_field(); ?>
-	<textarea class="embed_textarea" name="body" placeholder="Write report issue"></textarea>
-	<input type="hidden" name="video_id" value="<?php echo e($video->id); ?>">
-	<button type="submit" class="embed_action"><?php echo e(__('Send Report')); ?></button>
-</form>
-
-<script src="<?php echo e(asset('frontend/js/report.js')); ?>"></script><?php /**PATH C:\xampp\htdocs\newtong\script\resources\views/layouts/frontend/section/report.blade.php ENDPATH**/ ?>
--- script/storage/framework/views/d6c6dcb249ab192d543f7c2409b8e4e9e8e88f8f.php
+++
@@ -1,9 +0,0 @@
-<footer class="main-footer">
-	<div class="footer-left">
-		<?php echo e(__('Copyright')); ?> &copy; <?php echo e(date('Y')); ?> <div class="bullet"></div> <?php echo e(__('Developed By')); ?> <a href="#"><?php echo e(config('app.name')); ?></a>
-	</div>
-	<div class="footer-right">
-		<?php echo e(__('1.0.0')); ?>
-
-	</div>
-</footer><?php /**PATH C:\xampp\htdocs\newtong\script\resources\views/layouts/backend/partials/footer.blade.php ENDPATH**/ ?>
